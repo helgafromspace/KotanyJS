07da976b73bb167ca47bb84275eff423088e4318
olhasemenova <olha.sergeevna87@gmail.com>
2022-06-27 20:53:12 +0300

    code refactoring

--- checking_credentials/index.js
+++ checking_credentials/index.js
@@ -6,27 +6,27 @@ const credentials = {
     password: 'qwerty',
 };
 
-let button = document.querySelector(".button")
+let button = document.querySelector('.button');
 
-button.addEventListener("click",checkInput)
+button.addEventListener('click',checkInput);
 
-let login = document.querySelector('#login')
-let password = document.querySelector('#password')
-let loginForm = document.querySelector('.login-form')
-let message = document.querySelector('.hidden_message')
+let login = document.querySelector('#login');
+let password = document.querySelector('#password');
+let loginForm = document.querySelector('.login-form');
+let message = document.querySelector('.hidden_message');
 
 function checkInput(){
     if(login.value.toLowerCase() === credentials.login && password.value.toLowerCase() === credentials.password){
-        loginForm.style.borderColor = "green";
-        loginForm.style.borderWidth = "2px";
-        message.style.display = "block";
-        message.style.color = "green"
-        message.innerHTML = "Congratulations"
+        loginForm.style.borderColor = 'green';
+        loginForm.style.borderWidth = '2px';
+        message.style.display = 'block';
+        message.style.color = 'green';
+        message.innerHTML = 'Congratulations';
     } else {
-        loginForm.style.borderColor = "red";
-        loginForm.style.borderWidth = "2px";
-        message.style.display = "block";
-        message.style.color = "red"
-        message.innerHTML = "Wrong credentials!";
+        loginForm.style.borderColor = 'red';
+        loginForm.style.borderWidth = '2px';
+        message.style.display = 'block';
+        message.style.color = 'red';
+        message.innerHTML = 'Wrong credentials!';
     }
 }
